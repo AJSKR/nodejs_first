2b946eaf50c0a1920a2838896545198267594e1b
Ubuntu <ubuntu@ip-172-31-13-132.ap-northeast-2.compute.internal>
2020-12-09 07:54:04 +0000

    short impl

--- issuePushBot.js
+++ issuePushBot.js
@@ -1,10 +1,32 @@
 const { Gitlab } = require('@gitbeaker/node');
 
 const api = new Gitlab({
-    host: 'https://git.promedius.dev',
-    token: 'VX32ZQCRyxL16D4H1f66',
+  host: 'https://git.promedius.dev',
+  token: 'VX32ZQCRyxL16D4H1f66',
+});
+
+require('http').createServer((req,res)=>{
+  let body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  }).on('end', async ()=> {
+    let content;
+    try {
+      content = JSON.parse(Buffer.concat(body).toString());
+    } catch(e) {
+      console.error(e);
+    }
+    console.log(req.method, req.url, content);
+    const { title, description } = content;
+    if (title && description) {
+      await api.Issues.create(130, {
+        title, description
+      });
+    }
+    res.end("ok");
   });
-  
+}).listen(8080);
+
 /*
 HOW TO USE
 `curl -X POST localhost:8080 --data '{"title":"test #", "description":"works well"}'`
